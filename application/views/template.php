2a2abba8d436a7e26657977f94aad39659989f94
NurFadilatul <nurfadilatul11@gmail.com>
2021-10-02 10:51:42 +0700

    Update template.php

--- application/views/template.php
+++ application/views/template.php
@@ -343,7 +343,7 @@
         <footer class="footer">
           <div class="d-sm-flex justify-content-center justify-content-sm-between">
             <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Bootstrap admin template from BootstrapDash.</span>
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2021. All rights reserved.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright &copy; SI Peminjaman dan Pengembalian BRM RI RS Graha Sehat | NFF | <?= date('Y'); ?></span>
           </div>
         </footer>
       </div>
