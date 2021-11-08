1f829f39ee53204f69e65fdd5d3fecde0c069e94
Nugrahanto <rianirsyahnugrahanto@gmail.com>
2021-11-08 11:11:21 +0700

    Update Input Peminjaman
    Cek kondisi panjang char no rm

--- application/models/peminjamanModel.php
+++ application/models/peminjamanModel.php
@@ -26,31 +26,36 @@ class peminjamanModel extends CI_Model
         $strotime = strtotime($getdate);
         $date = date('Y-m-d', $strotime);
 
-        $check = $this->db->where('no_rm', $no_rm)
+        $countrm = strlen($no_rm);
+        if($countrm == 6){
+            $check = $this->db->where('no_rm', $no_rm)
                           ->where('tgl_kembali', null)
                           ->get('pengembalian')
                           ->row();
 
-        if (is_null($check)){
-            $data = array(
-                'id_peminjaman'  => NULL,
-                'no_rm'  => $no_rm,
-                'nama_pasien'    => $nama_pasien,
-                'tgl_lahir'    => $tgl_lahir,
-                'jekel'    => $jekel,
-                'ruangan'        => $ruangan,
-                'tgl_pinjam' => $date
-    
-            );
-            $this->db->insert('peminjaman', $data);
-    
-            if ($this->db->affected_rows() > 0) {
-                return 1;
+            if (is_null($check)){
+                $data = array(
+                    'id_peminjaman'  => NULL,
+                    'no_rm'  => $no_rm,
+                    'nama_pasien'    => $nama_pasien,
+                    'tgl_lahir'    => $tgl_lahir,
+                    'jekel'    => $jekel,
+                    'ruangan'        => $ruangan,
+                    'tgl_pinjam' => $date
+        
+                );
+                $this->db->insert('peminjaman', $data);
+        
+                if ($this->db->affected_rows() > 0) {
+                    return 1;
+                } else {
+                    return 0;
+                }
             } else {
-                return 0;
+                return 2;
             }
         } else {
-            return 2;
+            return 0;
         }
     }
     public function get_detail_data($id)
