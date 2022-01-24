2b8652f85919dd6282d736c1b7a1987ab4853fa0
NurFadilatul <nurfadilatul11@gmail.com>
2022-01-24 20:58:49 +0700

    Update datapeminjaman.php

--- application/views/rawatinap/datapeminjaman.php
+++ application/views/rawatinap/datapeminjaman.php
@@ -4,7 +4,7 @@
         <div class="col-12 grid-margin stretch-card">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="card-title">Tambah Peminjaman</h4>
+                    <h4 class="card-title">Tambah Data Peminjaman</h4>
                     <?php 
                     $message = $this->session->flashdata('message'); 
                     if (isset($message)) {
